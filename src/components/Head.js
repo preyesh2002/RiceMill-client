ff9287f58037ad020f52e8524c0e52955386e33a
preyesh2002 <119518427+preyesh2002@users.noreply.github.com>
2024-07-21 13:40:16 +0530

    css changes

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import myImage from './images/1.png'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import myImage from './images/1.png';
+import { Link } from 'react-router-dom';
 
-const head = () => {
+const Head = () => {
   return (
-    <div className="tale" >
-      <img src={myImage} alt="Logo" className="loogo"  />
+    <div className="tale">
+      <img src={myImage} alt="Logo" className="loogo" />
       <div className="enquiry">
-        <div  class="material-symbols-outlined">
-          phone_in_talk 
+        <div className="material-symbols-outlined">
+          phone_in_talk
         </div>
         <p>
-          +91-9481113939 <span>|</span> +91-9448976787<br></br>
+          +91-9481113939 <span>|</span> +91-9448976787<br />
           <a className="mail" href="mailto:enquiries@srivinayakaricemill.com">
-               enquiries@srivinayakaricemillricemill.com
+            enquiries@srivinayakaricemillricemill.com
           </a>
         </p>
-        <span></span>{' '}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default head
+export default Head;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,47 +1,46 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
+const Nav = () => {
+  const auth = localStorage.getItem('user');
+  const navigate = useNavigate();
 
+  const logout = () => {
+    localStorage.clear();
+    navigate('/signup');
+  };
 
-const Nav=()=>{
-  const auth=localStorage.getItem('user');
-  const navigate=useNavigate();
-const logout=()=>{
-
-  localStorage.clear();
-  navigate('/signup')
-}
-
-
-    return(
-    <div><nav class="navbar navbar-expand-lg bg-dark navbar-dark text-bg-light">
-    
-  
-  
-    <div class="container-fluid">
-      <Link class="navbar-brand" to="/">Sri Vinayaka Rice Mill</Link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-
-        { auth ? <>
-          {/* <Link to="/Home" class="nav-link active" aria-current="page" >Home</Link> */}
-          {/* <Link to="/Features" class="nav-link" >Features</Link> */}
-          <Link to="/ChartComponent" class="nav-link" >Inventory</Link>
-          <Link to="/AddProduct" class="nav-link" >Manage Stock</Link>
-          <Link to="/Transaction" class="nav-link" >Transaction</Link>
-          <Link onClick={logout} to ='/SignUp' class="nav-link">Logout</Link></>
-           :<><Link to="SignUp" class="nav-link" >SignUp</Link> <Link to="Login" class="nav-link" >Login</Link></>}
-          
-          
-          
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Sri Vinayaka Rice Mill</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav me-auto">
+            {auth ? (
+              <>
+                <Link to="/ChartComponent" className="nav-link">Inventory</Link>
+                <Link to="/AddProduct" className="nav-link">Manage Stock</Link>
+                <Link to="/Transaction" className="nav-link">Transaction</Link>
+                <Link onClick={logout} to='/SignUp' className="nav-link">Logout</Link>
+              </>
+            ) : (
+              <>
+                <Link to="SignUp" className="nav-link">SignUp</Link>
+                <Link to="Login" className="nav-link">Login</Link>
+              </>
+            )}
+          </div>
+          <div className="navbar-text text-nowrap">
+            <span className="contact-number">Contact: +1-234-567-890</span>
+          </div>
         </div>
       </div>
-    </div>
-  </nav></div>
-)}
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
